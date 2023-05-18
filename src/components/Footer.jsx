38f393c5d8742eea3842a082dffe2f9921d2f3d0
Kapil <choudharykapil94126@gmail.com>
2023-05-18 18:33:33 +0530

    add name in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import { Avatar, Box, Stack, VStack , Text } from '@chakra-ui/react';
 import React from 'react'
 
+const avatarSrc = "https://www.instagram.com/p/CKUTssxDx6C/";
+
 const Footer = () => {
   return (
     <Box bgColor={"blackAlpha.900"} color={"whiteAlpha.700"} minH={"48"} px={"16"} py={["16","8"]}>
@@ -10,8 +12,8 @@ const Footer = () => {
                 <Text fontSize={"sm"} letterSpacing={"widest"} textAlign={["center","left"]}>We are the best crypto trading app in India, We provide our guidance at a very reasonable price.</Text>
             </VStack>
             <VStack>
-                <Avatar boxSize={"28"} mt={["4","0"]} />
-                <Text>Our Founder</Text>
+                <Avatar boxSize={"28"} mt={["4","0"]} src={avatarSrc} />
+                <Text>Founded By Kapil Kumar</Text>
             </VStack>
         </Stack>
         
